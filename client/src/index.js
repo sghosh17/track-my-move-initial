c382403040b8e0fa2e999c08ec50d0b733c25b6e
pfansiW <fancyfreelance76@gmail.com>
2022-04-27 22:14:34 +0100

    #15 Wrapped the StepContext around BuyerHome page instead of globally on App.js - Header issue fixed

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import StepContext from "./StepContext";
 
 ReactDOM.render(
-  <StepContext>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </StepContext>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById("root")
 );
 
